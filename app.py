3a9b0f3f37ed4b8fd635150717fe538c51408ea5
OPApple <80098382+OPApple@users.noreply.github.com>
2022-09-14 12:25:51 +0300

    Added time conversion for python

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
+from xml.etree.ElementTree import tostring
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///times.db'
 db = SQLAlchemy(app)
+
+
+def toPythonDate(date):
+    new_date = str(date).split("-")
+    new_date_hms = new_date[2].split("T")
+
+    year = int(new_date[0])
+    month = int(new_date[1])
+    day = int(new_date_hms[0])
+    hour = int(new_date_hms[1].split(":")[0])
+    minute = int(new_date_hms[1].split(":")[1])
+
+    final_date = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
+    
+
+    return final_date
+
+def calculateDifference(date):
+    current_date = datetime.now()
+
+    time_difference =  date - current_date    
+
+    return time_difference
+    
+
+
 
 class Countdowns(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +40,10 @@ class Countdowns(db.Model):
 
     def __repr__(self):
         return self.content
+
+    # this allows getting an element (overrided method)
+    def __getitem__(self, id): 
+        return self.id
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -29,9 +60,11 @@ def index():
             return "Oops, fuck yuo"
         
     else:
-        countdowns = Countdowns.query.order_by(Countdowns.date_created).first()
+        countdowns = Countdowns.query.order_by(Countdowns.date_created.desc()).first()
+        countdowns = toPythonDate(countdowns)
+        app.logger.info(calculateDifference(countdowns))
+
         
-        app.logger.info(countdowns)
         return render_template('index.html', countdowns=countdowns)
         
 
